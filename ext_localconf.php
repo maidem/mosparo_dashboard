fb790fbc9fda2f45afeb28f85c13df42674ce716
maidem <maidem@users.noreply.github.com>
2026-07-30 19:43:09 +0200

    fix: rename TYPO3 extension key to mosparo_dashboard
    Der Extension Key mosparo_dashboard wurde jetzt bei TYPO3 gesichert,
    daher muss die eigene Erweiterung von maidem_mosparo_dashboard darauf umbenannt werden.

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,8 @@
 
 defined('TYPO3') or die();
 
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maidem_mosparo_dashboard'] ?? null)) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['maidem_mosparo_dashboard'] = [
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['mosparo_dashboard'] ?? null)) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['mosparo_dashboard'] = [
         'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
         'backend' => \TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend::class,
     ];
